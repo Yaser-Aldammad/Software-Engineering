96f5daefcde1ae1d33962828657758379bcf02c0
Pranav Arora <aurorapranav187@gmail.com>
2023-02-22 20:19:56 -0330

    Modify frontend file structure

--- frontend/index.js
+++ frontend/index.js
@@ -3,17 +3,6 @@ const app = express();
 const path = require("path");
 const port = 3000;
 
-// Set the template engine as pug
-// app.set("view engine", "pug");
-
-// // Set the pages directory
-
-// app.set("views", path.join(__dirname, "views"));
-
-// app.get("/base", (req, res) => {
-//   res.status(200).render("base", { title: "Pranav", message: "Some message" });
-// });
-
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "views/", "index.html"));
 });
