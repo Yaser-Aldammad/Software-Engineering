12cef4c1b1eb85896a26a99de5dee891981c1517
Yaser-Aldammad <yaser_dammad@hotmail.com>
2023-03-10 22:06:10 +0100

    update user

--- backend/v1/controllers/Users.js
+++ backend/v1/controllers/Users.js
@@ -6,18 +6,17 @@ const mailer = require('../helpers/mailer');
 const jwt = require(`jsonwebtoken`);
 const Usermodel = require('./models/UsersModel');
 
-
 /**
  * An object representing the controller.
  * @typedef {Object} Controller
-*/
+ */
 /**
  * An asynchronous function that validates a username by checking if it exists in the database.
  * @async
  * @function validateUserName
  * @param {string} name - The username to validate.
  * @returns {Promise<boolean>} A promise that resolves to a boolean indicating whether the username is valid or not.
-*/
+ */
 
 const controller = {};
 /**
@@ -26,16 +25,16 @@ const controller = {};
  * @function validateUserName
  * @param {string} name - The username to validate.
  * @returns {Promise<boolean>} A promise that resolves to a boolean indicating whether the username is valid or not.
-*/
+ */
 const validateUserName = async function (name) {
-  let result = false;
-  const p = ExampleModel.findOne({ username: name }).exec();
-  await p.then(user => {
-    if (user === null) {
-      result = true;
-    }
-  });
-  return result;
+	let result = false;
+	const p = UsersModel.findOne({ username: name }).exec();
+	await p.then((user) => {
+		if (user === null) {
+			result = true;
+		}
+	});
+	return result;
 };
 
 /**
@@ -49,33 +48,33 @@ const validateUserName = async function (name) {
  * @param {Object} req - Express request object containing the user's information.
  * @param {Object} res - Express response object containing the response to send back to the client.
  * @returns {Object} Returns a JSON object containing a success flag, a message, and user data with a JSON web token on successful registration.
-*/
+ */
 controller.AddUser = async function (req, res) {
-// Get user information from the request body and create a user object
-const user = {
-first_name: req.body.first_name,
-last_name: req.body.last_name,
-email: req.body.email,
-username: req.body.username,
-password: req.body.password,
-countryCode: req.body.countryCode,
-phoneNo: req.body.phoneNo,
-};
+	// Get user information from the request body and create a user object
+	const user = {
+		first_name: req.body.first_name,
+		last_name: req.body.last_name,
+		email: req.body.email,
+		username: req.body.username,
+		password: req.body.password,
+		countryCode: req.body.countryCode,
+		phoneNo: req.body.phoneNo,
+	};
 
-// If the username is defined, truncate it to 60 characters
-if (typeof user.username !== `undefined`) {
+	// If the username is defined, truncate it to 60 characters
+	if (typeof user.username !== `undefined`) {
 		user.username = user.username.slice(0, 60);
 	}
 
-// Check if the username already exists, return error if it does
-if ((await validateUserName(user.username)) === false) {
+	// Check if the username already exists, return error if it does
+	if ((await validateUserName(user.username)) === false) {
 		return res
 			.status(400)
 			.json({ success: false, message: `That username already exists` });
 	}
 
-// Check if the email already exists, return error if it does
-const email_exist = await Usermodel.findOne({ email: user.email });
+	// Check if the email already exists, return error if it does
+	const email_exist = await Usermodel.findOne({ email: user.email });
 
 	if (email_exist) {
 		return res
@@ -83,16 +82,16 @@ const email_exist = await Usermodel.findOne({ email: user.email });
 			.json({ success: false, message: `That email already exists` });
 	}
 
-// Create a new user and save it to the database
-const model = new UsersModel(user);
+	// Create a new user and save it to the database
+	const model = new UsersModel(user);
 	const promise = model.save();
 
-// Create a JSON web token using the user's ID and the server's secret key
-const token = jwt.sign({ sub: model._id }, settings.server.secret, {
+	// Create a JSON web token using the user's ID and the server's secret key
+	const token = jwt.sign({ sub: model._id }, settings.server.secret, {
 		algorithm: 'HS512',
 	});
 
-// Return a success response with the newly created user object and JSON web token
+	// Return a success response with the newly created user object and JSON web token
 	promise
 		.then((user) => {
 			let resp = {
@@ -104,7 +103,7 @@ const token = jwt.sign({ sub: model._id }, settings.server.secret, {
 		})
 		.catch((ex) => {
 			console.log(ex);
-	// Return an error response if there was an error saving the user
+			// Return an error response if there was an error saving the user
 			let resp = {
 				success: false,
 				message: 'error',
@@ -113,4 +112,59 @@ const token = jwt.sign({ sub: model._id }, settings.server.secret, {
 		});
 };
 
+controller.GetUsersList = function (req, res) {
+	UsersModel.find({}, (err, users) => {
+		if (err) {
+			res.status(400).json({
+				success: false,
+				message: `Something went wrong. Please try again later`,
+			});
+		} else {
+			res.json({
+				success: true,
+				message: `users listed successfully`,
+				data: users,
+			});
+		}
+	});
+};
+
+controller.GetUserProfile = function (req, res) {
+	const query = UsersModel.findById(req.params.id);
+	const promise = query.exec();
+	promise
+		.then((user) => {
+			res.status(200).json({ success: true, message: `Success`, data: user });
+		})
+		.catch((ex) => {
+			res.status(400).json({ success: false, message: `error` });
+		});
+};
+
+controller.UpdateUser = async function (req, res) {
+	UsersModel.findById(req.params.id)
+		.then(async (user) => {
+			if (user === null) {
+				throw `User not found with that ID`;
+			}
+			user.first_name = req.body.first_name || user.first_name;
+			user.last_name = req.body.last_name || user.last_name;
+			user.username = req.body.username || user.username;
+			user.password = req.body.password || user.password;
+			user.email = req.body.email || user.email;
+			user.picture = req.body.picture || user.picture;
+			user.phoneNo = req.body.phoneNo || user.phoneNo;
+			user.picture = req.body.picture || user.picture;
+
+			return await user.save();
+		})
+		.then((user) => {
+			res
+				.status(200)
+				.json({ success: true, message: `Successfuly updated`, data: user });
+		})
+		.catch((ex) => {
+			res.status(500).json({ success: false, message: `error` });
+		});
+};
 module.exports = controller;
--- backend/v1/routes/router-public.js
+++ backend/v1/routes/router-public.js
@@ -1,36 +1,42 @@
 const router = require(`express`).Router();
-const { Router } = require("express");
+const { Router } = require('express');
 const _auth = require(`../auth`);
 const users = require(`../controllers/Users`);
 /**
  * Public router
- * @param {*} database 
- * @param {*} settings 
+ * @param {*} database
+ * @param {*} settings
  * @returns {Router}
  */
 module.exports = function RouterPublic(database, settings) {
-    const db = database;
-    const auth = _auth();
+	const db = database;
+	const auth = _auth();
 
-    // Auth
-    /**
-     * routes GET /login to route to the login pages
-     */
-    router.get(`/login`, auth.isUserAuthenticated, async (req, res) => {
-        console.log('test')
-        res.status(200).json({
-            success: true,
-            message: "Login successful",
-            data: req.user
-        });
-    });
-    /**
-     * /signup this will route to the signup page  
-     */
-    router.route(`/signup`).post(users.AddUser);
-    //router.route(`/upload/:id`).post(upload.single("img"), users.uploadImage);
-    router.route(`/forget-password`).post(users.ForgetPassword);
-    router.route(`/forget-password-verify`).post(users.ForgetPasswordVerify);
+	// Auth
+	/**
+	 * routes GET /login to route to the login pages
+	 */
+	router.get(`/login`, auth.isUserAuthenticated, async (req, res) => {
+		console.log('test');
+		res.status(200).json({
+			success: true,
+			message: 'Login successful',
+			data: req.user,
+		});
+	});
+	/**
+	 * /signup this will route to the signup page
+	 */
+	router.route(`/signup`).post(users.AddUser);
 
-    return router;
+	// Get all users endpoint
+	router.route(`/users`).get(users.GetUsersList);
+
+	// Get single user endpoint
+	router.route(`/user/:id`).get(users.GetUserProfile);
+
+	// update single user endpoint
+	router.route(`/user/:id`).put(users.UpdateUser);
+
+	return router;
 };
