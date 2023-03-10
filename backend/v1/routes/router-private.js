10095ba31bb507900613faf8d1d3de3061cfd5e5
prabinKshrestha <prabin2026@gmail.com>
2023-03-10 12:43:25 -0330

    Features under User Story Quiz API complete [#42 #43 #44 #45 #46]: Includes CRUD operation for the Quiz

--- backend/v1/routes/router-private.js
+++ backend/v1/routes/router-private.js
@@ -2,6 +2,7 @@ const router = require(`express`).Router();
 const users = require(`../controllers/Users`);
 const { Router } = require("express");
 const authentication = require("../middleware/validateJWT");
+const quizController = require(`../controllers/QuizController`);
 
 
 // const CollectionMulerSettings = upload.fields([
@@ -16,6 +17,32 @@ const authentication = require("../middleware/validateJWT");
  */
 module.exports = function RouterPrivate(database, settings) {
   router.use(authentication.authenticate);
+
+
+    //#region Quiz
+  
+    /**
+     * routes POST /quiz to get add quiz
+     */
+    router.route(`/quiz`).post(quizController.AddQuiz);
+    /**
+     * routes PATCH /quiz/:quizId to update quiz
+     */
+    router.route(`/quiz/:quizId`).patch(quizController.UpdateQuiz);
+    /**
+     * routes DELETE /quiz/:quizId to delete quiz
+     */
+    router.route(`/quiz/:quizId`).delete(quizController.DeleteByQuizId);
+    /**
+     * routes GET /quiz/:quizId to get quiz by quizId
+     */
+    router.route(`/quiz/:quizId`).get(quizController.GetQuizById);
+    /**
+     * routes GET /quiz to get all quizzes
+     */
+    router.route(`/quiz`).get(quizController.GetAllQuizes);
+  
+    //#endregion
 
     /**
      * routes GET /logout and clear the session cookie
