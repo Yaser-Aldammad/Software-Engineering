5b058648c87e491b47ae9b3a372237519c885457
Balsher Singh <balshersingh10@gmail.com>
2023-03-24 20:52:52 -0230

    update signup and resolve bugs

--- frontend/assets/js/signupform.js
+++ frontend/assets/js/signupform.js
@@ -1,37 +1,40 @@
-const form = document.querySelector('#signup');
-form.addEventListener('submit', async function(event) {
+const form = document.querySelector("#signup");
+form.addEventListener("submit", async function (event) {
   event.preventDefault();
-  handleForm().then(data => {
-    localStorage.setItem('token',data.data.token)
-    const token = localStorage.getItem('token')
-    console.log(token)
-  })
-  .catch(error => {
-    console.error(error)
-  });
+  handleForm()
+    .then((data) => {
+      localStorage.setItem("token", data.data.token);
+      const token = localStorage.getItem("token");
+      console.log(token);
+      window.location.replace("./index.html");
+    })
+    .catch((error) => {
+      console.error(error);
+    });
 });
 
 async function handleForm() {
-    const firstName = document.getElementById('firstName').value;
-    const lastName = document.getElementById('lastName').value;
-    const email = document.getElementById('email').value;
-    const password = document.getElementById('password').value;
+  const username = document.getElementById("username").value;
+  const lastName = document.getElementById("lastName").value;
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+  const countryCode = "+"+document.getElementById("countrycode").value.toString();;
+  const phoneNo = document.getElementById("phone").value;
 
-    const res = await fetch('http://localhost:3000/v1/signup',
-    {
-        method:'POST',
-        headers:{
-            "Content-Type":'application/json'
-        },
-        body: JSON.stringify({
-            first_name:firstName,
-            last_name:lastName,
-            email:email,
-            username:firstName,
-            password:password,
-            countryCode:"+92",
-            phoneNo:"9812451245"
-        })
-    })
-    return res.json();
+  const res = await fetch("http://localhost:3000/v1/signup", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      first_name: username,
+      last_name: lastName,
+      email: email,
+      username: username,
+      password: password,
+      countryCode: countryCode,
+      phoneNo: phoneNo,
+    }),
+  });
+  return res.json();
 }
